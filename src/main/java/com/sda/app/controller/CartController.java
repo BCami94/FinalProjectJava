e428727732325ecb9371e91363be041f7b8de57c
Camelia B <camelia.bordencea17@gmail.com>
2023-11-12 09:15:02 +0200

    update for final project 11/11/2023

--- src/main/java/com/sda/app/controller/CartController.java
+++ src/main/java/com/sda/app/controller/CartController.java
@@ -1,2 +1,59 @@
-package com.sda.app.controller;public class CategoryController {
+package com.sda.app.controller;
+
+import com.sda.app.entity.Cart;
+import com.sda.app.entity.User;
+import com.sda.app.service.CartService;
+import com.sda.app.utils.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+    private CartService cartService;
+
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> getAllCarts() {
+           List<Cart> cartList = this.cartService.findAll();
+           ApiResponse response = new ApiResponse.Builder()
+                .status(200)
+                .message("List de cosuri")
+                .data(cartList)
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> createCart(Cart cart) {
+        ApiResponse response = new ApiResponse.Builder()
+                .status(200)
+                .message("Cos creat cu success")
+                .data(cartService.createCart(cart))
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApiResponse> updateCart(Cart cart) {
+        ApiResponse response = new ApiResponse.Builder()
+                .status(200)
+                .message("Cos actualizat cu success")
+                .data(cartService.updateCart(cart))
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<ApiResponse> deleteCart(Cart cart) {
+        cartService.deleteCart(cart);
+        ApiResponse response = new ApiResponse.Builder()
+                .status(200)
+                .message("Cos sters cu success")
+                .data(null)
+                .build();
+        return ResponseEntity.ok(response);
+    }
 }
+
